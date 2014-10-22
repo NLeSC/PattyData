43bd2fbc5889a3e0e2924ea2606cc95293d04571
elboyran <e.ranguelova@esciencecenter.nl>
2014-10-22 16:57:08 +0200

    skeleton of the JSON creation script

--- python/createjson.py
+++ python/createjson.py
@@ -4,43 +4,8 @@
 #            and Elena Ranguelova                                              #
 #    o.rubi@esciencecenter.nl                                                  #
 ################################################################################
-#import os, argparse, psycopg2, time, re, multiprocessing, glob, logging, shutil, subprocess
-import os, argparse, time, glob, json. logging
+import os, argparse, time, glob, json, logging
 import utils 
-
-# The DATA folder must have the following structure:
-#
-# DATA
-# |- BACKGROUND
-# |  |- RAW
-#        |- pc1
-#            |- data
-#            |- pc1.json EXAMPLE: {"srid": 32633, "max": [0, 0, 0], "numberpoints": 20000000, "extension": "laz", "min": [0, 0, 0], "t_x" : None, ...}
-#        |- pc2
-# ...
-# |  \- CONV
-#        |- pc1
-#            |- pc1v1
-#               |- data
-#               |- pc1v1.js
-#            |- pc1v2
-#            ...
-#        |- pc2
-# ...
-# \- SITES
-#     |-  1
-#     ... |-PIC
-#         |  |- CURR
-#         |  \- HIST
-#         |- MESHES     
-#         |  |- CURR
-#         |  \- ARCH_RECONS
-#         |- PC     
-#         |  |- RAW
-#         |  \- CONV
-#                 |- pc1
-#                 |- pc2
-#                 ...
 
 # CONSTANTS
 DEFAULT_DB = 'vadb'
@@ -52,7 +17,6 @@ def argument_parser():
     """ Define the arguments and return the parser object"""
     parser = argparse.ArgumentParser(
     description="Run the script to populate the database")
-    #formatter_class=argparse.RawTextHelpFormatter) 
     parser.add_argument('-o','--output',help='Output JSON file',type=str, required=True)
     parser.add_argument('-d','--dbname',default=DEFAULT_DB,help='PostgreSQL DB name where to store the geometries [default ' + DEFAULT_DB + ']',type=str , required=True)
     parser.add_argument('-u','--dbuser',default=username,help='DB user [default ' + username + ']',type=str, required=True)
@@ -72,13 +36,9 @@ def apply_argument_parser(options=None):
     return args
 
 def run(args):    
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt="%Y/%m/%d/%H:%M:%S", level=getattr(logging, opts.log))
-    t0 = time.time()
-    # Check options
-        
-    # Absolute data path
-    dataAbsPath = os.path.abspath(args.data)
-    
+     t0 = time.time()
+           
+       
     # Global variables declaration
     global connection
     
@@ -90,8 +50,7 @@ def run(args):
     
     rows = cursor.fetchall()
     
-    rows = [[162,],[13,]]
-    
+        
     jsonData = {}
     jsonData["type"] = "FeatureCollection"
     featuresList = []
@@ -104,49 +63,8 @@ def run(args):
         featuresList.append(siteFeaturesDict)
         
     jsonData["features"] = featuresList
-    jsonData["crs"] = {}
-    
-    # Process the backgrounds
-    backgroundsAbsPath = os.path.join(dataAbsPath,'BACKGROUND')
-    rawBackgroundsAbsPath = os.path.join(backgroundsAbsPath,'RAW')
-    convBackgroundsAbsPath = os.path.join(backgroundsAbsPath,'CONV')
-    
-    rawBackgrounds = sorted(os.listdir(rawBackgroundsAbsPath))
-    convBackgrounds = sorted(os.listdir(convBackgroundsAbsPath))
-    
-    # Check that there are not backgrounds in converted which are not in raw
-    for convBackground in convBackgrounds:
-        if convBackground not in rawBackgrounds:
-            logging.error('background ' + convBackground + ' not found in ' + rawBackgroundsAbsPath)
-    
-    checkedTime = utils.getCurrentTime()
-            
-    for rawBackground in rawBackgrounds:
-        if rawBackground not in convBackgrounds:
-            logging.warning('background ' + rawBackground + ' not found in ' + convBackgroundsAbsPath)
-        rawBackgroundAbsPath = os.path.join(rawBackgroundsAbsPath, rawBackground)
-        modTime = utils.getCurrentTime(utils.getLastModification(rawBackgroundAbsPath))
-        jsonFiles = glob.glob(os.path.join(rawBackgroundAbsPath, '*json'))
-        if len(jsonFiles) != 1:
-            logging.error(' background ' + rawBackground + ' does not contain JSON file!')
-        else:
-            jsonAbsPath = os.path.join(rawBackgroundAbsPath,rawBackground)
-            jsonData = json.loads(open(jsonFiles[0],'r').read())
-            # {"srid": 32633, "max": [0, 0, 0], "numberpoints": 20000000, "extension": "laz", "min": [0, 0, 0]}
-            utils.dbExecute(cursor, 'SELECT pc_id, last_mod FROM pc WHERE folder = %s', [rawBackgroundAbsPath,])
-            row = cursor.fetchone()
-            if row == None: #This folder has been added recently
-                values = []
-                utils.dbExecute(cursor, 'INSERT INTO pc (pc_id, srid, numberpoints, folder, extension, last_mod,last_check,minx,miny,minz,maxx,maxy,maxz) VALUES (DEFAULT,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING pc_id', values)
-                    
-                
-    
-    #{"srid": 32633, "max": [0, 0, 0], "numberpoints": 20000000, "extension": "laz", "min": [0, 0, 0]}
-    
-    #for background in backgrounds:
-    #    backgroundAbsPath = os.path.join(backgroundsAbsPath, background)
-        
-    
+    jsonData["crs"] = {}         
+   
     
     
     print 'Finished!. Total time ', time.time() - t0
