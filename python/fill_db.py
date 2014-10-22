6c98c4231cc2eda64815532840ecbf66bfd6efce
“oscar.martinez.rubi” <omrubi@gmail.com>
2014-10-22 13:27:44 +0200

    modified fill_db

--- python/fill_db.py
+++ python/fill_db.py
@@ -3,8 +3,8 @@
 #    Created by Oscar Martinez                                                 #
 #    o.rubi@esciencecenter.nl                                                  #
 ################################################################################
-#import os, optparse, psycopg2, time, re, multiprocessing, glob, logging, shutil, subprocess
-import os, optparse, time
+#import os, argparse, psycopg2, time, re, multiprocessing, glob, logging, shutil, subprocess
+import os, argparse, time, glob
 import utils 
 
 # The DATA folder must have the following structure:
@@ -13,10 +13,15 @@ import utils
 # |- BACKGROUND
 # |  |- RAW
 #        |- pc1
+#            |- data
+#            |- pc1.json EXAMPLE: {"srid": 32633, "max": [0, 0, 0], "numberpoints": 20000000, "extension": "laz", "min": [0, 0, 0]}
 #        |- pc2
 # ...
 # |  \- CONV
 #        |- pc1
+#            |- pc1v1
+#            |- pc1v2
+#            ...
 #        |- pc2
 # ...
 # \- SITES
@@ -66,7 +71,14 @@ def main(opts):
     rawBackgrounds = sorted(os.listdir(rawBackgroundsAbsPath))
     convBackgrounds = sorted(os.listdir(convBackgroundsAbsPath))
     
+    for rawBackground in rawBackgrounds:
+        if rawBackground not in convBackgrounds:
+            print 'ERROR: background ' + rawBackground + ' not found in ' + convBackgroundsAbsPath
+        rawBackgroundAbsPath = os.path.join(rawBackgroundsAbsPath,rawBackground)
+        glob.glob()
     
+    
+    #{"srid": 32633, "max": [0, 0, 0], "numberpoints": 20000000, "extension": "laz", "min": [0, 0, 0]}
     
     #for background in backgrounds:
     #    backgroundAbsPath = os.path.join(backgroundsAbsPath, background)
@@ -81,12 +93,12 @@ username = os.popen('whoami').read().replace('\n','')
 if __name__ == "__main__":
     usage = 'Usage: %prog [options]'
     description = "Fills the DB"
-    op = optparse.OptionParser(usage=usage, description=description)
-    op.add_option('-i','--data',help='Data folder',type='string', nargs='+')
-    op.add_option('-d','--dbname',default=DEFAULT_DB,help='PostgreSQL DB name where to store the geometries [default ' + DEFAULT_DB + ']',type='string' , nargs='+')
-    op.add_option('-u','--dbuser',default=username,help='DB user [default ' + username + ']',type='string', nargs='+')
-    op.add_option('-p','--dbpass',default='',help='DB pass',type='string', nargs='+')
-    op.add_option('-t','--dbhost',default='',help='DB host',type='string', nargs='+')
-    op.add_option('-r','--dbport',default='',help='DB port',type='string', nargs='+')
+    op = argparse.ArgumentParser(usage=usage, description=description)
+    op.add_argument('-i','--data',help='Data folder',type=str, required=True)
+    op.add_argument('-d','--dbname',default=DEFAULT_DB,help='PostgreSQL DB name where to store the geometries [default ' + DEFAULT_DB + ']',type=str , required=True)
+    op.add_argument('-u','--dbuser',default=username,help='DB user [default ' + username + ']',type=str, required=True)
+    op.add_argument('-p','--dbpass',default='',help='DB pass',type=str, required=True)
+    op.add_argument('-t','--dbhost',default='',help='DB host',type=str, required=True)
+    op.add_argument('-r','--dbport',default='',help='DB port',type=str, required=True)
     (opts, args) = op.parse_args()
     main(opts)
