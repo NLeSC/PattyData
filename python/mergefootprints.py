8a53b8d8f5e4a7f325ac6b745843ec770f22ef99
Oscar Martinez Rubi <omrubi@gmail.com>
2015-01-14 17:21:39 +0100

    working mergefootprints

--- python/mergefootprints.py
+++ python/mergefootprints.py
@@ -8,9 +8,12 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt=
 import utils 
         
 def main(opts):
+    if opts.input == '':
+        logging.error('specify input file!')
+        return
     # Check that beginning of the file does not contain a create database statement
-    if os.popen('head -500 ' + opts.input + ' | grep "CREATE TABLE sites_geom_temp"').read().count("CREATE TABLE sites_geom_temp") == 0:
-        logging.error("The table in the SQL file must be named sites_geom_temp. Replace the table name to sites_geom_temp")
+    if os.popen('head -500 ' + opts.input + ' | grep "CREATE TABLE sites_geoms_temp"').read().count("CREATE TABLE sites_geoms_temp") == 0:
+        logging.error("The table in the SQL file must be named sites_geom_temp. Replace the table name to sites_geoms_temp")
         return    
      
     # Establish connection with DB
@@ -33,7 +36,7 @@ def main(opts):
         cursor.execute("ALTER TABLE sites_geoms ALTER COLUMN geom TYPE geometry(MultiPolygon," + str(utils.SRID) + ")")
         connection.commit()
         #cursor.execute("insert into sites_geoms select site_id,st_astext(geom) from (select site_id, geom, row_number() over (partition by site_id) as rownum from sites_geom_temp) tmp where rownum < 2")
-        cursor.execute("INSERT INTO sites_geoms( site_id, geom ) SELECT site_id as site_id, ST_Multi(ST_Transform( ST_Union( geom ), " + str(utils.SRID) + " )) AS geom FROM sites_geom_temp GROUP BY site_id")
+        cursor.execute("INSERT INTO sites_geoms( site_id, geom ) SELECT site_id as site_id, ST_Multi(ST_Transform( ST_Union( geom ), " + str(utils.SRID) + " )) AS geom FROM sites_geoms_temp GROUP BY site_id")
         
         connection.commit()
         cursor.execute("DROP TABLE sites_geoms_temp")
@@ -44,7 +47,8 @@ if __name__ == "__main__":
     description = "Merges Footprints file (SQL) with ViaAppia DB"
     op = optparse.OptionParser(usage=usage, description=description)
     op.add_option('-i','--input',default='',help='SQL file with SQL footprints',type='string')
-    op.add_option('-d','--dbname',default=utils.DEFAULT_DB,help='Postgres DB name where to store the geometries [default ' + utils.DEFAULT_DB + ']',type='string')
+#    op.add_option('-d','--dbname',default=utils.DEFAULT_DB,help='Postgres DB name where to store the geometries [default ' + utils.DEFAULT_DB + ']',type='string')
+    op.add_option('-d','--dbname',default='temppattydb',help='Postgres DB name where to store the geometries [default temppattydb]',type='string')
     op.add_option('-u','--dbuser',default=utils.USERNAME,help='DB user [default ' + utils.USERNAME + ']',type='string')
     op.add_option('-p','--dbpass',default='',help='DB pass',type='string')
     op.add_option('-t','--dbhost',default='',help='DB host',type='string')
