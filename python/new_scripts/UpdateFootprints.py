174820bac5b4e3eba796e56c99f1d8e7bc3e84e1
elboyran <e.ranguelova@esciencecenter.nl>
2015-01-22 11:35:07 +0100

    header of UpdateFootprints.py

---
+++ python/new_scripts/UpdateFootprints.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+################################################################################
+# Description:      Script to update the sites footprints in the DB 
+#                   which are imported from an sql file (another DB dump)
+# Author:           Elena Ranguelova, NLeSc, E.Ranguelova@nlesc.nl                                       
+# Creation date:    22.01.2015      
+# Modification date:
+# Modifications:   
+# Notes:            Based on mergefootprints.py from the ViaAppia project
+################################################################################
