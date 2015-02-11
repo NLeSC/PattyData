8abb8f9f43a30d55d444a87c1dada30c8a367d41
Ronald van Haren <r.vanharen@esciencecenter.nl>
2015-02-11 10:05:53 +0100

    remove aligned option; add srid option; bug fixes

--- python/new_scripts/AddRawDataItem.py
+++ python/new_scripts/AddRawDataItem.py
@@ -78,12 +78,12 @@ def check_required_options(opts):
                          utils.HIST_FT + "' (--period)")
     # SITES should have a site number defined
     if (opts.kind == utils.SITE_FT):
-        if not (opts.siteno):
+        if not (opts.site):
             logger.error(
                 "Site number should be defined for " + utils.SITE_FT +
-                " (--siteno)")
+                " (--site)")
             parser.error("Site number should be defined for " + utils.SITE_FT +
-                         " (--siteno)")
+                         " (--site)")
 
 
 def check_directory_structure(RAW_BASEDIR):
@@ -113,7 +113,8 @@ def check_input_data(opts):
     """
     logger.info('Checking input data.')
     # name of Raw Data item may not contain (CURR, BACK, OSG)
-    if any(substring in opts.file for substring in ['CURR', 'BACK', 'OSG']):
+    if any(substring in os.path.basename(opts.file) for substring
+           in ['CURR', 'BACK', 'OSG']):
         logger.error("Input data may not contain (CURR, BACK, OSG)")
         raise IOError("Input data may not contain (CURR, BACK, OSG)")
     # All pictures must have JSON file with same name (.png.json)
@@ -124,7 +125,7 @@ def check_input_data(opts):
             src_files = os.listdir(opts.file)
             for file_name in src_files:
                 # check for figures in directory
-                if (os.path.splitext(testname)[1][1:].lower() in ['png',
+                if (os.path.splitext(file_name)[1][1:].lower() in ['png',
                                                                   'jpg',
                                                                   'jpeg']):
                     if (os.path.isfile(os.path.join(opts.file,
@@ -136,10 +137,10 @@ def check_input_data(opts):
                                        "input: " + file_name)
         # if input is a file and is indeed a figure:
         elif (os.path.isfile(opts.file) and os.path.splitext(
-              testname)[1][1:].lower() in ['png', 'jpg', 'jpeg']):
+              opts.file)[1][1:].lower() in ['png', 'jpg', 'jpeg']):
             # check for json file ${filename}.json
-            if (opts.file + '.json'):
-                check_json_file(opts.filee + '.json')
+            if os.path.isfile(opts.file + '.json'):
+                check_json_file(opts.file + '.json')
             else:
                 logger.warning("No accompanying JSON file found for input: " +
                                opts.file)
@@ -174,40 +175,38 @@ def check_json_file(jsonfile):
         raise Exception("json file should contain at least (x,y,z,srid)")
 
 
-def alignment_8bitcolor_info(inputname, opts):
+def srid_8bitcolor_info(inputname, opts):
     """
-    Defines 8bit / alignment options
+    Defines 8bit / srid options
     """
-    eightbitinfo, alignmentinfo = "", ""  # default
+    eightbitinfo, sridinfo = "", ""  # default
     if (opts.eight and (opts.type == utils.MESH_FT or
                         (opts.type == utils.PC_FT and opts.kind ==
                          utils.SITE_FT))):
-        # check if inputname already contains alignment information
+        # check if inputname already contains 8bc information
         if any(substring in inputname.lower() for substring in ['8bit',
                                                                 '8bc']):
             # 8bitcolor info already in folder name
             pass
         else:
             eightbitinfo = "_8BC"
-    if (opts.aligned and opts.kind == utils.SITE_FT and (opts.type ==
-                                                         utils.PC_FT or
-                                                         opts.type ==
-                                                         utils.MESH_FT)):
-        # check if inputname already contains alignment information
-        if any(substring in inputname.lower() for substring in ['aligned']):
-            # check if alignment info in inputname is correct
-            if opts.aligned not in inputname:
-                logger.error('Alignment info in filename does ' +
-                             'not match specified alignment argument.')
-                raise Exception('Alignment info in filename ' +
-                                'does not match specified alignment ' +
+    if (opts.srid and opts.kind == utils.SITE_FT and (opts.type ==
+                                                      utils.MESH_FT)):
+        # check if inputname already contains srid information
+        if any(substring in inputname.lower() for substring in ['srid']):
+            # check if srid info in inputname is correct
+            if opts.srid not in inputname:
+                logger.error('SRID info in filename does ' +
+                             'not match specified srid argument.')
+                raise Exception('SRID info in filename ' +
+                                'does not match specified srid ' +
                                 'argument.')
             else:
                 pass
         else:
-            alignmentinfo = "_ALIGNED_"+opts.aligned
-    al8bit = alignmentinfo+eightbitinfo
-    return al8bit
+            sridinfo = "_SRID_"+opts.srid
+    srid8bit = sridinfo+eightbitinfo
+    return srid8bit
 
 
 def define_create_target_dir(opts):
@@ -218,44 +217,44 @@ def define_create_target_dir(opts):
     target_basedir = os.path.join(opts.data, opts.type, opts.kind)
     # name of input data, only basename, extensions removed
     inputname = os.path.splitext(os.path.basename(opts.file))[0]
-    al8bit = alignment_8bitcolor_info(inputname, opts)
+    srid8bit = srid_8bitcolor_info(inputname, opts)
 
     # TARGETDIR for PC
     if (opts.type == utils.PC_FT and opts.kind == utils.BG_FT):
         TARGETDIR = os.path.join(
             target_basedir, inputname)
     if (opts.type == utils.PC_FT and opts.kind == utils.SITE_FT):
-        if (opts.aligned):
-            # check if background used for the alignment exists
-            if (os.path.isdir(os.path.join(
-                              opts.data, utils.PC_FT, utils.BG_FT,
-                              os.path.splitext(
-                                  os.path.basename(opts.aligned))[0]))):
-                alignedTo = os.path.splitext(os.path.basename(opts.file))[0]
-            else:
-                logger.error('Alignment background does not exist: ' +
-                             os.path.splitext(os.path.basename(
-                                 opts.aligned))[0])
-                raise IOError('Alignment background does not exist: ' +
-                              os.path.splitext(os.path.basename(
-                                  opts.aligned))[0])
-        TARGETDIR = os.path.join(target_basedir, 'S'+str(opts.siteno))
+#        if (opts.aligned):
+#            # check if background used for the alignment exists
+#            if (os.path.isdir(os.path.join(
+#                              opts.data, utils.PC_FT, utils.BG_FT,
+#                              os.path.splitext(
+#                                  os.path.basename(opts.aligned))[0]))):
+#                alignedTo = os.path.splitext(os.path.basename(opts.file))[0]
+#            else:
+#                logger.error('Alignment background does not exist: ' +
+#                             os.path.splitext(os.path.basename(
+#                                 opts.aligned))[0])
+#                raise IOError('Alignment background does not exist: ' +
+#                              os.path.splitext(os.path.basename(
+#                                  opts.aligned))[0])
+        TARGETDIR = os.path.join(target_basedir, 'S'+str(opts.site))
 
     # TARGETDIR for MESH
     if (opts.type == utils.MESH_FT and opts.kind == utils.BG_FT):
         TARGETDIR = os.path.join(target_basedir,
                                  opts.period,
-                                 inputname+al8bit)
+                                 inputname+srid8bit)
     if (opts.type == utils.MESH_FT and opts.kind == utils.SITE_FT):
         TARGETDIR = os.path.join(target_basedir, opts.period,
-                                 'S'+str(opts.siteno),
-                                 inputname+al8bit)
+                                 'S'+str(opts.site),
+                                 inputname+srid8bit)
     # TARGETDIR for PICT
     if (opts.type == utils.PIC_FT and opts.kind == utils.BG_FT):
         TARGETDIR = os.path.join(target_basedir, opts.period)
     if (opts.type == utils.PIC_FT and opts.kind == utils.SITE_FT):
         TARGETDIR = os.path.join(target_basedir, opts.period,
-                                 'S'+str(opts.siteno))
+                                 'S'+str(opts.site))
     # check if TARGETDIR exists, create otherwise
     if not os.path.isdir(TARGETDIR):
         os.makedirs(TARGETDIR)  # create directories recursively
@@ -280,26 +279,28 @@ def copy_data(opts, TARGETDIR):
     # copy everything inside the directory to TARGETDIR
     if os.path.isdir(opts.file):
         src_files = os.listdir(opts.file)
-        if opts.type == utils.PC_FT and opts.kind == utils.SITE_FT:
+        # for PC SITE and PICT put all files in their own subdirectory
+        if ((opts.type == utils.PC_FT and opts.kind == utils.SITE_FT
+             ) or (opts.type == utils.PIC_FT)):
             for file_name in src_files:
                 basedir = os.path.splitext(file_name)[0]
                 if (os.path.isfile(os.path.join(opts.file, file_name))):
-                    # check 8bit/alignment info already in filename
+                    # check 8bit/srid info already in filename
                     # so we don't duplicate it in the folder name
-                    al8bit = alignment_8bitcolor_info(file_name, opts)
+                    srid8bit = srid_8bitcolor_info(file_name, opts)
                     if not os.path.isdir(os.path.join
-                                         (TARGETDIR, basedir + al8bit)):
-                        os.mkdir(os.path.join(TARGETDIR, basedir + al8bit))
+                                         (TARGETDIR, basedir + srid8bit)):
+                        os.mkdir(os.path.join(TARGETDIR, basedir + srid8bit))
                         shutil.copyfile(os.path.join(opts.file, file_name),
                                         os.path.join(TARGETDIR, basedir +
-                                                     al8bit, file_name))
+                                                     srid8bit, file_name))
                     else:
                         logger.error(os.path.join
-                                     (TARGETDIR, basedir + al8bit) +
-                                     'already exists, exiting.')
-                        raise IOERROR(os.path.join
-                                      (TARGETDIR, basedir + al8bit) +
-                                      'already exists, exiting.')
+                                     (TARGETDIR, basedir + srid8bit) +
+                                     ' already exists, exiting.')
+                        raise IOError(os.path.join
+                                      (TARGETDIR, basedir + srid8bit) +
+                                      ' already exists, exiting.')
         else:
             for file_name in src_files:
                 if (os.path.isfile(os.path.join(opts.file, file_name))):
@@ -308,7 +309,7 @@ def copy_data(opts, TARGETDIR):
         # if input was a file:
         # copy the file to TARGETDIR
         # create a directory name from the filename
-        basedir = os.path.splitext(opts.file)[0]
+        basedir = os.path.basename(os.path.splitext(opts.file)[0])
         os.makedirs(os.path.join(TARGETDIR, basedir))
         # copy the data
         shutil.copyfile(opts.file, os.path.join(TARGETDIR, basedir,
@@ -391,16 +392,16 @@ if __name__ == "__main__":
                                       ',' + utils.HIST_FT + ')',
                                       choices=[utils.CURR_FT, utils.HIST_FT,
                                                utils.ARCREC_FT])
-    parser.add_argument('-a', '--aligned', action='store',
-                        help='Aligned to a specific background [' +
-                        'only for PC,MESH SITE]')
+    parser.add_argument('-s', '--srid', action='store',
+                        help='spatial reference system SRID [' +
+                        'only for MESH SITE]')
     parser.add_argument('--eight', help='8 bit color [only for PC SITE or ' +
                         'MESH]', action="store_true")
     parser.add_argument('-l', '--log', help='Log level',
                         choices=['debug', 'info', 'warning', 'error',
                                  'critical'],
                         default=utils.DEFAULT_LOG_LEVEL)
-    requiredNamedSITE.add_argument('--siteno', action='store',
+    requiredNamedSITE.add_argument('--site', action='store',
                                    type=int, help='Site number')
     # extract user entered arguments
     opts = parser.parse_args()
