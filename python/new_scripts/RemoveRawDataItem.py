76704a566173ec296c6774f78dbec284c9cb8c6f
elboyran <e.ranguelova@esciencecenter.nl>
2015-02-20 18:12:28 +0100

    skeleton of RemoveDataItem with arguments and argument parser

--- python/new_scripts/RemoveRawDataItem.py
+++ python/new_scripts/RemoveRawDataItem.py
@@ -3,9 +3,9 @@
 # Description:      Script to remove a raw data item and the related POTree/OSG
 #
 # Authors:          Oscar Martinez, NLeSC, o.rubi@esciencecenter.nl
-#
+#                   Elena Ranguelova, NLeSc
 # Created:          16.02.2015
-# Last modified:    16.02.2015
+# Last modified:    20.02.2015
 #
 # Changes:
 #
@@ -14,57 +14,83 @@
 #                   * The absPath of related (OSG/POTree) data item are retrieved
 #                   * All the previous data is deleted
 ##############################################################################
-import re
-import os
-import shutil
 import argparse
 import utils
-import json
-import liblas
-import glob
+
 
 logger = None
+connection = None
+cursor = None
 
-def remove_data(opts):
-    """
-    Removes the data from the file structure.
-    """
-    logger.info('Removing data.')
-    logger.info("Finished copying data to " + TARGETDIR)
-
-def main(opts):
-    # set logging level
-    global logger
-    logger = utils.start_logging(filename=utils.LOG_FILENAME, level=opts.log)
-    logger.info('#######################################')
-    logger.info('Starting script RemoveRawDataItem.py')
-    logger.info('#######################################')
-
-    # copy the data to the target directory
-    remove_data(opts)
-
-
-if __name__ == "__main__":
-    # define argument menu
+def argument_parser():
     description = "Removes a Raw data item and the related converted data from the file structure."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-i', '--itemid', help='Raw data item id (with ? the available raw data items are listed)',
                         action='store', required=True)
-    parser.add_argument('-d', '--dbname', default=utils.DEFAULT_DB,
-                        help='Postgres DB name [default ' + utils.DEFAULT_DB +
-                        ']', action='store')
-    parser.add_argument('-u', '--dbuser', default=utils.USERNAME,
-                        help='DB user [default ' + utils.USERNAME +
-                        ']', action='store')
-    parser.add_argument('-p', '--dbpass', help='DB pass', action='store')
-    parser.add_argument('-t', '--dbhost', help='DB host', action='store')
-    parser.add_argument('-r', '--dbport', help='DB port', action='store')
+    parser.add_argument('-d','--dbname',default=utils.DEFAULT_DB, help='PostgreSQL DB name which should be updated with these footprints ' + utils.DEFAULT_DB + ']',type=str , required=False)
+    parser.add_argument('-u','--dbuser',default=utils.USERNAME,help='DB user [default ' + utils.USERNAME + ']',type=str, required=True)
+    parser.add_argument('-p','--dbpass',default='',help='DB pass',type=str, required=True)
+    parser.add_argument('-t','--dbhost',default='',help='DB host',type=str, required=True)
+    parser.add_argument('-r','--dbport',default='',help='DB port',type=str, required=False)
     parser.add_argument('-l', '--log', help='Log level',
                         choices=['debug', 'info', 'warning', 'error',
                                  'critical'],
                         default=utils.DEFAULT_LOG_LEVEL)
-    # extract user entered arguments
-    opts = parser.parse_args()
 
-    # run main
-    main(opts)
+    return parser 
+ 
+def apply_argument_parser(options=None):
+    """ Apply the argument parser. """
+    parser = argument_parser()
+    if options is not None:
+        args = parser.parse_args(options)
+    else:
+        args = parser.parse_args() 
+            
+    return args
+    
+#def remove_data(opts):
+#    """
+#    Removes the data from the file structure.
+#    """
+#    logger.info('Removing data.')
+#   # logger.info("Finished copying data to " + TARGETDIR)
+
+
+def fetch_abs_path():
+    """ get the absolute data item path given the site ID"""
+    abs_path = ""
+    
+    
+    
+    return abs_path
+    
+#------------------------------------------------------------------------------        
+def run(args): 
+    
+    # set logging level
+    global logger
+    global connection
+    global cursor
+    
+    logger = utils.start_logging(filename=utils.LOG_FILENAME, level=args.log)
+    logger.info('#######################################')
+    logger.info('Starting script RemoveRawDataItem.py')
+    logger.info('#######################################')
+
+ # start timer
+    t0 = utils.getCurrentTime()
+    
+    # connect to the Db
+     # connect to the DB
+    connection, cursor = utils.connectToDB(args.dbname, args.dbuser, args.dbpass, args.dbhost, args.dbport) 
+        
+    # copy the data to the target directory
+    #remove_data(opts)
+
+    return
+
+
+    
+if __name__ == '__main__':
+    run( apply_argument_parser() )
