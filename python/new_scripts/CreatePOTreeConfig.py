01ed2a28238357d8dce1f12c8cf9086c4fd19069
“oscar.martinez.rubi” <omrubi@gmail.com>
2015-02-23 17:48:34 +0100

    Changes in create potree config (to be tested)

--- python/new_scripts/CreatePOTreeConfig.py
+++ python/new_scripts/CreatePOTreeConfig.py
@@ -19,6 +19,7 @@ def argument_parser():
     parser = argparse.ArgumentParser(
     description="Script to generate a JSON file from the ViaAppiaDB for the ViaAppia POtree web-based visualization")
     parser.add_argument('-o','--output',help='Output JSON file [Log of operation is stored in [output].log]',type=str, required=True)
+    parser.add_argument('-s','--srid',default=utils.SRID, help='SRID used in POtree visualization [default ' + utils.SRID + ']',type=int , required=True)
     parser.add_argument('-d','--dbname',default=utils.DEFAULT_DB, help='PostgreSQL DB [default ' + utils.DEFAULT_DB + ']',type=str , required=True)
     parser.add_argument('-u','--dbuser',default=utils.USERNAME,help='DB user [default ' + utils.USERNAME + ']',type=str, required=True)
     parser.add_argument('-p','--dbpass',default='',help='DB pass',type=str, required=False)
@@ -38,126 +39,163 @@ def apply_argument_parser(options=None):
     return args
 
 def addThumbnail(cursor, itemId, jsonSite):
-    query = 'SELECT A.abs_path, B.thumbnail FROM raw_data_item A, raw_data_item_picture B WHERE A.raw_data_item_id == B.raw_data_item_id AND A.item_id = %s'
+    query = 'SELECT A.abs_path, B.thumbnail FROM raw_data_item A, raw_data_item_picture B WHERE A.raw_data_item_id = B.raw_data_item_id AND A.item_id = %s'
     queryArgs = [itemId,]
-    images, num_images = utils.fetchDataFromDB(cursor, query,  queryArgs)
-    if num_images:
+    site_images, num_site_images = utils.fetchDataFromDB(cursor, query,  queryArgs)
+    if num_site_images:
         imageAbsPath = None
         # We use the thumbnail if available
-        for (absPath, thumbnail) in images:
+        for (absPath, thumbnail) in site_images:
             if thumbnail:
                 imageAbsPath = absPath
         # If no thumbnail is available we just use the first image
         if imageAbsPath == None:
-            (absPath, thumbnail) = images[0] 
+            (absPath, thumbnail) = site_images[0] 
             imageAbsPath = absPath
-        jsonSite["thumbnail"] = utils.POTREE_DATA_URL_PREFIX + imageAbsPath.replace(utils.POTREE_SERVER_DATA_ROOT,'')
+        jsonSite["thumbnail"] = utils.POTREE_DATA_URL_PREFIX + imageAbsPath.replace(utils.POTREE_SERVER_DATA_ROOT,'') + os.listdir(imageAbsPath)[0]
+    else:
+        logger.warning('No image found for item %d' % itemId)
 
-def addSiteMetaData(cursor, itemId, jsonSite):
-    query = 'SELECT description_site, site_context, site_interpretation FROM tbl1_site WHERE A.raw_data_item_id == B.raw_data_item_id AND A.item_id = %s'
+def addSiteMetaData(cursor, itemId, dataSite):
+    query = 'SELECT description_site, site_context, site_interpretation FROM tbl1_site WHERE site_id = %s'
     queryArgs = [itemId,]
-    images, num_images = utils.fetchDataFromDB(cursor, query,  queryArgs)
+    site_metadatas, num_site_metadatas = utils.fetchDataFromDB(cursor, query,  queryArgs)
     
-    jsonSite["description_site"] = "Pyramid"
-    jsonSite["site_context"] = "Funerary"
-    jsonSite["site_interpretation"] = "Funerary tower"
-    return
+    if num_site_metadatas:
+        (descriptionSite, siteContext, siteInterpretation) = site_metadatas[0]
+        dataSite["description_site"] = descriptionSite
+        dataSite["site_context"] = siteContext
+        dataSite["site_interpretation"] = siteInterpretation
+    else:
+        logger.warning('No meta-data found for item %d' % itemId)
 
+def addPointCloud(cursor, itemId, dataSite, srid):
+    query = 'SELECT C.abs_path, B.minx, B.miny, B.minz, B.maxx, B.maxy, B.maxz FROM raw_data_item A, raw_data_item_pc B, potree_data_item_pc WHERE A.raw_data_item_id = B.raw_data_item_id AND B.raw_data_item_id = C.raw_data_item_id AND A.item_id = %s AND A.srid = %s'
+    queryArgs = [itemId,srid]
+    site_pcs, num_site_pcs = utils.fetchDataFromDB(cursor, query,  queryArgs)
+    if num_site_pcs:
+        (pcAbsPath, pcMinx, pcMiny, pcMinz, pcMaxx, pcMaxy, pcMaxz) = site_pcs[0] # We only use first PC
+        dataSite["pointcloud_bbox"] = [pcMinx, pcMiny, pcMinz, pcMaxx, pcMaxy, pcMaxz]
+        dataSite["pointcloud"] = utils.POTREE_DATA_URL_PREFIX + absPath.replace(utils.POTREE_SERVER_DATA_ROOT,'') + "/cloud.js"
+    else:
+        logger.warning('No potree point cloud found for item %d SRID %d' % (itemId, srid))
 
-def addPointCloud(cursor, itemId, jsonSite):
-    return
-def addMeshes(cursor, itemId, jsonSite):
-    return
-def addObjectsMetaData(cursor, itemId, jsonSite):
-    return
+def getOSGPosition(srid, osgLocationSRID, x, y, z, xs, ys, zs, h, p, r):
+    osgPosition = {}
+    if srid == osgLocationSRID:
+        osgPosition['x'] = x
+        osgPosition['y'] = y
+        osgPosition['z'] = z
+        osgPosition['xs'] = xs
+        osgPosition['ys'] = ys
+        osgPosition['zs'] = zs
+        osgPosition['h'] = h
+        osgPosition['p'] = p
+        osgPosition['r'] = r
+    else:
+        if osgLocationSRID != None:
+            logger.warning('Found SRID %d (instead of %d). Treating it as None SRID' % (osgLocationSRID, srid))
+        # We assume the osg location is relative
+        # We need to make it absolute by adding the offset of the background with srid as provided
+        query = 'SELECT C.offset_x, C.offset_y, C.offset_z from raw_data_item A, raw_data_item_pc B, osg_data_item_pc_background C WHERE A.raw_data_item_id = B.raw_data_item_id AND B.raw_data_item_id = C.raw_data_item_id AND A.srid = %s'
+        queryArgs = [srid,]
+        backgroundOffsets, num_backgrounds = utils.fetchDataFromDB(cursor, query,  queryArgs)
+        (offsetX,offsetY,offsetZ) = (0,0,0)
+        if num_backgrounds:
+            (offsetX,offsetY,offsetZ) = backgroundOffsets[0]
+        osgPosition['x'] = x + offsetX
+        osgPosition['y'] = y + offsetY
+        osgPosition['z'] = z + offsetZ
+        osgPosition['xs'] = xs
+        osgPosition['ys'] = ys
+        osgPosition['zs'] = zs
+        osgPosition['h'] = h
+        osgPosition['p'] = p
+        osgPosition['r'] = r
+    return osgPosition
 
-def create_features_json(cursor, pc_ids, args):
-    global jsonData
+def addMeshes(cursor, itemId, dataSite, srid):
+    query = """
+SELECT 
+    A.abs_path, B.mtl_abs_path, B.current_mesh, E.srid, 
+    E.x, E.y, E.z, E.xs, E.ys, E.zs, E.h, E.p, E.r
+FROM 
+    raw_data_item A, raw_data_item_mesh B, osg_data_item_mesh C, 
+    osg_data_item D, osg_location E 
+WHERE 
+    A.raw_data_item_id = B.raw_data_item_id AND
+    B.raw_data_item_id = C.raw_data_item_id AND
+    C.osg_data_item_id = D.osg_data_item_id AND
+    D.osg_location_id = E.osg_location_id AND
+    A.item_id = %s"""
+    queryArgs = [itemId,]
+
+    site_meshes, num_site_meshes = utils.fetchDataFromDB(cursor, query,  queryArgs)
     
-    relative_to_path = args.location
+    meshData = None
+    recMeshesData = []
     
-       # features per sites
-    featuresList = []
+    if num_site_meshes:
+        for (absPath, mtlAbsPath, current, meshSrid, x, y, z, xs, ys, zs, h, p ,r) in site_meshes:
+            if not current or (current and meshData == None):
+                mData = {}
+                mData["data_location"] = utils.POTREE_DATA_URL_PREFIX + absPath.replace(utils.POTREE_SERVER_DATA_ROOT,'') + (os.listdir(absPath + '/*.obj') + os.listdir(absPath + '/*.OBJ'))[0]
+                if mtlAbsPath != None:
+                    mData["data_location"] = utils.POTREE_DATA_URL_PREFIX + mtlAbsPath.replace(utils.POTREE_SERVER_DATA_ROOT,'')
+                mData['osg_position'] = getOSGPosition(srid, meshSrid, x, y, z, xs, ys, zs, h, p, r)
+                if current:
+                    meshData = mData
+                else:
+                    mData['id'] = len(recMeshesData) + 1
+                    recMeshesData.append(mData)
+    else:
+        logger.warning('No meshes found for item %d SRID %d' % (itemId, srid))
     
-    for pc in pc_ids:
-        
-        # initialize the features data structure        
-        siteFeaturesDict = {}
-        
-        siteFeaturesDict["type"] = "Feature"
-        siteFeaturesDict["geometry"]={}
-        (siteId,) = pc       
-        siteFeaturesDict["id"] = siteId
-        siteFeaturesDict["properties"]={}
-        siteFeaturesDict["bbox"]=""
-        
-        properties={}
-        properties["pointcloud"]=""
-        properties["description"]=""
-        properties["thumbnail"]=""
-        properties["site_context"]=""
-        properties["site_interpretation"]=""
-        properties["condition"]=""
-        
-        # fetch the BBox from the DB
-        sql_statement ='select minx, miny, minz, maxx, maxy, maxz from raw_data_item_pc where raw_data_item_id = %s', [siteId,]        
-        cursor.execute(sql_statement)
-        if (cursor.rowcount >0):
-            (minx, miny, minz, maxx, maxy, maxz) = cursor.fetchone()
-        siteFeaturesDict["bbox"] = [minx, miny, minz, maxx, maxy, maxz]
-        
-        # fetch the geometry of the site from the DB
-        sql_statement = 'select st_asgeojson(geom::geometry,15,4) from raw_data_item_pc natural join raw_data_item natural join item where raw_data_item_id = %s', [siteId,]
-        cursor.execute(sql_statement)  
-        if (cursor.rowcount >0):
-            data = cursor.fetchone()[0]
-            json_data = json.loads(data)
-            json_data_crs = json_data['crs'];
-            json_data_geometry= dict(json_data)
-            json_data_geometry.pop('crs')
-        
-        
-            siteFeaturesDict["geometry"] = json_data_geometry
-            siteFeaturesDict["crs"] = json_data_crs
-        
-        # fetch the properties of the site from the DB 
-        sql_statement = 'select abs_path from raw_data_item natural join raw_data_item_pc where (raw_data_item_id = %s',[siteId,]
-        cursor.execute(sql_statement)
-        if (cursor.rowcount >0):
-             abs_path =  cursor.fetchone()[0]
-             rel_path = abs_path.replace(relative_to_path,'')
-             properties["pointcloud"] = rel_path
-        
-        sql_statement = 'select description_site, site_context, site_interpretation from tbl1_site natural join raw_data_item where raw_data_item_id = %s', [siteId,]
-        cursor.execute(sql_statement)
-        if (cursor.rowcount >0):
-            (description, context, interpretation) = cursor.fetchone()
-            properties["description"] = description
-            properties["site_context"] = context
-            properties["site_interpretation"] = interpretation
-        
-        sql_statement = 'select abs_path from raw_data_item natural join raw_data_item_picture where (thumbnail=True) and (raw_data_item_id = %s)', [siteId,]
-        cursor.execute(sql_statement)
-        if (cursor.rowcount >0):
-             abs_path =  cursor.fetchone()[0]
-             rel_path = abs_path.replace(relative_to_path,'')
-             properties["thumbnail"] = rel_path
+    if meshData != None:
+        dataSite["mesh"] = meshData
+    dataSite["reconstruction_mesh"] = recMeshesData
 
+def addObjectsMetaData(cursor, itemId, jsonSite, srid):
+    query = 'SELECT object_id, in_situ, ancient, condition, description_object, object_type, object_interpretation, period, date_specific, description_restorations FROM tbl1_object WHERE site_id = %s'
+    queryArgs = [itemId,]
+    
+    site_objects, num_site_objects = utils.fetchDataFromDB(cursor, query,  queryArgs)
+    
+    objectsData = []
+    
+    for (object_id, in_situ, ancient, condition, description_object, object_type, object_interpretation, period, date_specific, description_restorations) in site_objects:
+        objectData = {}
+        objectData['object_id'] = object_id
+        objectData['in_situ'] = in_situ
+        objectData['ancient'] = ancient
+        objectData['condition'] = condition
+        objectData['description_object'] = description_object
+        objectData['object_type'] = object_type
+        objectData['object_interpretation'] = object_interpretation
+        objectData['period'] = period
+        objectData['date_specific'] = date_specific
+        objectData['description_restorations'] = description_restorations
         
-        siteFeaturesDict["properties"]=properties
-        
-        # add to the features
-        featuresList.append(siteFeaturesDict)
-        
-    jsonData["features"] = featuresList 
-
-
-    msg = 'Created the features in JSON format.'
-    print(msg)
-    logger.debug(msg)
-    pretty_json = json.dumps(jsonData, indent=4, separators=(',', ': '))
-    print(pretty_json)
-    logger.debug(pretty_json)    
+        query = "SELECT B.srid, B.x, B.y, B.z, B.xs, B.ys, B.zs, B.h, B.p, B.r FROM osg_item_object A, osg_location B WHERE A.item_id = %s AND A.object_number = %s"
+        queryArgs = [itemId,object_id]
+        object_locations, num_object_locations = utils.fetchDataFromDB(cursor, query,  queryArgs)
+        if num_object_locations:
+            (locationSrid, x, y, z, xs, ys, zs, h, p ,r) = object_locations[0]
+            objectData['osg_position'] = getOSGPosition(srid, locationSrid, x, y, z, xs, ys, zs, h, p, r)
+            objectsMaterialData= []
+            query = "SELECT material_type, material_subtype, material_technique FROM tbl2_object_material  WHERE site_id = %s AND object_id = %s"
+            queryArgs = [itemId,object_id]
+            object_materials, num_object_materials = utils.fetchDataFromDB(cursor, query,  queryArgs)
+            for (material_type, material_subtype, material_technique) in object_materials:
+                objectMaterialData = {}
+                objectMaterialData["id"] = len(objectsMaterialData) + 1
+                objectMaterialData["material_type"] = material_type
+                objectMaterialData["material_subtype"] = material_subtype
+                objectMaterialData["material_technique"] = material_technique
+                objectsMaterialData.append(objectMaterialData)
+            objectData["object_material"] = objectsMaterialData
+        objectsData.append(objectData)
+    jsonSite["objects"] = objectsData
     
 def save2JSON(outFileName, jsonData):
     with open(outFileName, 'w') as outfile:
@@ -186,27 +224,27 @@ def run(args):
     query = 'SELECT item_id, ST_ASGEOJSON(geom) FROM item WHERE NOT background'        
     sites, num_sites = utils.fetchDataFromDB(cursor, query)
     
-    jsonData = []
+    data = []
     
     for (itemId, itemGeom) in sites:
         # Generate the JSON data for this item
-        jsonSite = {}
-        jsonSite["id"] = itemId
-        jsonSite["footprint"] = json.loads(itemGeom)['coordinates']
+        dataSite = {}
+        dataSite["id"] = itemId
+        dataSite["footprint"] = json.loads(itemGeom)['coordinates']
         
-        addThumbnail(cursor, itemId, jsonSite)
-        addSiteMetaData(cursor, itemId, jsonSite)
-        addPointCloud(cursor, itemId, jsonSite)
-        addMeshes(cursor, itemId, jsonSite)
-        addObjectsMetaData(cursor, itemId, jsonSite)
+        addThumbnail(cursor, itemId, dataSite)
+        addSiteMetaData(cursor, itemId, dataSite)
+        addPointCloud(cursor, itemId, dataSite, args.srid)
+        addMeshes(cursor, itemId, dataSite, srid)
+        addObjectsMetaData(cursor, itemId, dataSite, srid)
         
-        jsonData.append(jsonSite)
+        data.append(dataSite)
         
     # close the Db connection
     utils.closeConnectionDB(connection, cursor)    
 
     # save the data into JSON file
-    save2JSON(args.output, jsonData)
+    save2JSON(args.output, data)
     
     elapsed_time = utils.getCurrentTime() - t0    
     msg = 'Finished. Total elapsed time: %s s.' %elapsed_time
